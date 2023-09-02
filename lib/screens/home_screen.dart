45be52be25b85895293c505e44b778ea0e11f453
Daniel Ntege <danientege785@gmail.com>
2023-09-02 15:24:01 +0300

    Hoem Screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,8 +1,20 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Color(0xFFeeedf2),
+      body: ListView(
+        children: [
+          Container(
+            child: Column(),
+          )
+        ],
+      ),
+    );
   }
 }
